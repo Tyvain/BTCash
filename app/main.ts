fe3d3b1fac3dac4ef8fb504e82772ea29b88c694
Rafaliya Haresh <rafaliyaharesh@yahoo.in>
2016-12-07 16:33:52 +0530

    last commit

--- app/main.ts
+++
@@ -1,6 +0,0 @@
-import {bootstrap} from "angular2/platform/browser";
-import { HTTP_PROVIDERS } from 'angular2/http';
-
-import { DemoComponent }  from './demo';
-
-bootstrap(DemoComponent, [HTTP_PROVIDERS])
